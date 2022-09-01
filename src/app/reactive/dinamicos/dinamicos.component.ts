e7245495db47f2660c61ced2adaf72a85ba02a49
Martín <magadiflo@gmail.com>
2022-08-31 23:58:26 -0500

    FormArray

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -11,7 +11,16 @@ export class DinamicosComponent {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: [null, [Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array([ //* <--- Este sí es un arreglo
+      ['Metal Gear', [Validators.required]],                      //* <<-- ['Metal Gear'...], no son arreglos, sino colleciones de formControls
+      ['Death Stranding', [Validators.required]],
+      this.fb.control('winning level 8', [Validators.required])    //* <<-- esto sería lo mismo que ['Metal Gear'...], es decir los formControls sería llamando desde el formBuilder (fb)
+    ], [Validators.required])
   });
+
+  get favoritosArray() {
+    return (this.miFormulario.get('favoritos') as FormArray);
+  }
 
   constructor(private fb: FormBuilder) { }
 
