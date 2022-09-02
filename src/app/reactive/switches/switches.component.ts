22021f3eadab748d1bbaec438f65b9499661c6d7
Martín <magadiflo@gmail.com>
2022-09-02 00:01:08 -0500

    Formularios Reactivos: Switches

--- src/app/reactive/switches/switches.component.ts
+++ src/app/reactive/switches/switches.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches',
@@ -8,9 +9,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SwitchesComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * * Validators.requiredTrue, importante para validar un campo de tipo check ya que esta validación obliga a que el campo esté con check. 
+   * * El Validators.required, no aplicaría aquí ya que esta dice si el campo tiene valor o no
+   */
+
+  miFormulario: FormGroup = this.fb.group({
+    genero: ['M', [Validators.required]],
+    notificaciones: [false, [Validators.required]],
+    terminos: [false, [Validators.requiredTrue]]
+  });
+
+  persona = {
+    genero: 'F',
+    notificaciones: true,
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    /**
+     * * terminos: false, le agregamos esta propiedad ya que persona solo tiene 2 propiedades.
+     * * Faltaría agregarle el término ya que si no lo hacemos ese campo lo establecería en null.
+     * * Entonces, para ser explícitos le decimos que cuando inicie el formulario el campo terminos
+     * * (al no estar checkeado) inicie en false y no en null
+     */
+    this.miFormulario.reset({
+      ...this.persona,
+      terminos: false 
+    });
   }
 
 }
