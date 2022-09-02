b813c839115eb3eebd094518ac72f7c6e1f399c8
Martín <magadiflo@gmail.com>
2022-09-01 23:15:05 -0500

    Agregar controles al FormArray

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, FormArray } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -18,6 +18,8 @@ export class DinamicosComponent {
     ], [Validators.required])
   });
 
+  nuevoFavorito: FormControl = this.fb.control('', [Validators.required]); //* Necesario agregar la validación para que cuando se de en el botón agregar, éste se evalúe con el if y no pase
+
   get favoritosArray() {
     return (this.miFormulario.get('favoritos') as FormArray);
   }
@@ -34,6 +36,17 @@ export class DinamicosComponent {
 
   isFieldInvalid(field: string) {
     return this.miFormulario.controls[field].touched && this.miFormulario.controls[field].errors;
+  }
+
+  agregarFavorito(): void {
+    if (this.nuevoFavorito.invalid) return;
+
+    //* No mandamos directamente al arreglo el nuevoFavorito ya que podríamos mantener la referencia de la variable dentro del arreglo
+    //* y eso podría traer problemas más adelante. Entonces, se opta por crear un nuevo objeto (formControl) y pasarle los valores del nuevoFavorito
+
+    //* this.favoritosArray.push(new FormControl(this.nuevoFavorito.value, [Validators.required])); //* Usando el new FormControl
+    this.favoritosArray.push(this.fb.control(this.nuevoFavorito.value, [Validators.required])); //* Usando el FormBuilder para crear un new FormControl
+    this.nuevoFavorito.reset();
   }
 
 }
