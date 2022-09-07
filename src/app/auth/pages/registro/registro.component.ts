6fe6799cafcc8f3e334aa4e1ab67f5aa192cde91
Martín <magadiflo@gmail.com>
2022-09-06 22:46:41 -0500

    Validaciones personalizadas

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -13,10 +13,21 @@ export class RegistroComponent implements OnInit {
   nombreApellidoPattern: string = '([a-zA-ZáéíóúÁÉÍÓÚ]+) ([a-zA-ZáéíóúÁÉÍÓÚ]+)';
   emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
+  //* Creando una validación SÍNCRONA
+  noPuedeSerStrider(control: FormControl) {
+    const valor: string = control.value?.trim().toLowerCase();
+    if(valor === 'strider'){
+      return { //* Con que nosotros regresemos un objeto en nuestra validación, esta se considera como un error
+        noStrider: true,
+      }
+    }
+    return null;//* Si retornamos null en una validación significa que no hay error, todo está bien
+  }
+
   miFormulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
     email: ['', [Validators.required, Validators.pattern(this.emailPattern)]],
-    username: [],
+    username: ['', [Validators.required, this.noPuedeSerStrider]],
     password: []
   });
 
@@ -26,6 +37,7 @@ export class RegistroComponent implements OnInit {
     this.miFormulario.reset({
       nombre: 'Martín Díaz',
       email: 'test@correo.com',
+      username: 'magadiflo',
     });
   }
 
